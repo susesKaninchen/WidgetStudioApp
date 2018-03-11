1d6bdb02645c60c20821c39e99ea64740c9682f0
susesKaninchen <m.gabrecht@gmx.de>
2018-03-11 13:55:48 +0100

    InfoText erweitert

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Sabai-Sabai',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Sabai-Sabai'),
@@ -29,16 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,18 +32,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // ################################Drawer Element
+    // ######################################################################## Drawer Element
     Drawer firstDrawer = new Drawer(
         child: new ListView(children: <Widget>[new Row(
           children: <Widget>[
@@ -87,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.black))),
         ],));
     // #####################################################
-    // #####################################################Termine Tabel Row
+    // ######################################################################## Termine Tabel Row
     List<TableRow> termin = new List<TableRow>();
     termin.add(
         new TableRow(
@@ -190,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: new ListView(
           children: <Widget>[
-            new Row(
+            ("" == "s" ? new Row(
               children: <Widget>[
                 new Container(
                     decoration: new BoxDecoration(
@@ -216,7 +193,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Row Element
 
                 // Row End
-              ],),
+              ],) : new Text("")),
             tableView
           ],
         ),
@@ -262,17 +239,22 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildExpandExpandsionTitle() {
-    return new Column(children: <Widget>[new Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        new Text("Termin Absagen"),
-        new Text("Termin vershcieben"),
-        new Icon(Icons.dehaze)
-      ],),
-    new Text("Termin Annehmen"),
-    new Text("Termin ablehnen"),
-    new Text("Termin ist zugesagt")
-    ],);
+    return new Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[new Column(
+
+        children: <Widget>[
+          new Text("Datum: 10.10.2018"),
+          new Text("Urzeit: 18:00-19:00"),
+          new Text("Gebucht am: 9.10.2018"),
+          new Text("Bestätigt am: 9.10.2018"),
+        ],),
+      new Text("Termin bei: Helga Sabai"),
+      new Text("Termin ablehnen"),
+      new Text("Termin ist zugesagt"),
+        new Column(children: <Widget>[_buildSeparation("Absagen"),
+        _buildSeparation("Bestätigen"),
+        _buildSeparation("Bemerkung hinzufügen")],)
+      ],);
   }
 
   Container _buildSeparation(String text) {
