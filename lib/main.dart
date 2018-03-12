aa2b73a1522cd58a022953f330a2e35f8ef69357
susesKaninchen <m.gabrecht@gmx.de>
2018-03-12 18:12:44 +0100

    Farben und input und Switch zwischen Bestatigen und Absagen

--- lib/main.dart
+++ lib/main.dart
@@ -41,46 +41,46 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // ######################################################################### Table
     Widget tableView = new Column(children: <Widget>[
+      new ListTile(
+        title: new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            new Container(width: 80.0,
+              child: new Text("Uhrzeit", style: new TextStyle(
+                  fontSize: 20.0, fontWeight: FontWeight.bold)),),
+            new Expanded(child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[new Text(
+                    "Massageart (lange in Minuten)", style: new TextStyle(
+                    fontSize: 20.0, fontWeight: FontWeight.bold)),
+                new Text("Kundenname", style: new TextStyle(
+                    fontSize: 20.0, fontWeight: FontWeight.bold)),
+                new Row(
+                  children: <Widget>[
+                    new Icon(Icons.face, size: 30.0,),
+                    new Container(margin: new EdgeInsets.only(
+                        right: 12.5, left: 12.5),
+                      child: new Icon(
+                        Icons.monetization_on, size: 30.0,),),
+                    new Icon(Icons.build)
+                  ],)
+                ])),
+          ],),),
       _buildSeparation("Heute"),
       //new Table(children: termin, border: new TableBorder(horizontalInside: new BorderSide(color: Colors.grey , width: 1.0))),
       new Column(
           children: <Widget>[
-            new ListTile(
-              title: new Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  new Container(width: 80.0,
-                    child: new Text("Uhrzeit", style: new TextStyle(
-                        fontSize: 20.0, fontWeight: FontWeight.bold)),),
-                  new Expanded(child: new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[new Text(
-                          "Massageart (lange in Minuten)", style: new TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold)),
-                      new Text("Kundenname", style: new TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold)),
-                      new Row(
-                        children: <Widget>[
-                          new Icon(Icons.face, size: 30.0,),
-                          new Container(margin: new EdgeInsets.only(
-                              right: 12.5, left: 12.5),
-                            child: new Icon(
-                              Icons.monetization_on, size: 30.0,),),
-                          new Icon(Icons.build)
-                        ],)
-                      ])),
-                ],),),
             new Container(decoration: new BoxDecoration(
                 border: new Border(bottom: new BorderSide())),),
             _buildExpansionTitle(
                 "18:00\n-19:00", "Ölmassage (60 min)", "Marco Hansen",
-                false, _buildExpandExpandsionTitle()),
+                false, 0, _buildExpandExpandsionTitle(true)),
             _buildExpansionTitle(
                 "19:00\n-19:30", "Ölmassage (30 min)", "Peter Hansen",
-                true, _buildExpandExpandsionTitle()),
+                true, 1, _buildExpandExpandsionTitle(true)),
             _buildExpansionTitle(
                 "19:30\n-20:00", "Kräuterstempelmassage (30 min)",
-                "Harrison Ford", true, _buildExpandExpandsionTitle()),
+                "Harrison Ford", true, 3, _buildExpandExpandsionTitle(false)),
           ]
       ),
       _buildSeparation("Morgen"),
@@ -100,52 +100,63 @@ class _MyHomePageState extends State<MyHomePage> {
                         margin: new EdgeInsets.all(5.0),
                         color: Colors.grey,
                         child: new RaisedButton(
-                          child: new Text("Termin\nerstellen", style: new TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),textAlign: TextAlign.center), onPressed: () {},
+                          child: new Text(
+                              "Termin\nerstellen", style: new TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                              textAlign: TextAlign.center), onPressed: () {},
                         )),
                     new Container(
                         margin: new EdgeInsets.all(5.0),
                         color: Colors.grey,
-                        child:new RaisedButton(
-                            child: new Text("Studio\nbearbeiten", style: new TextStyle(
+                        child: new RaisedButton(
+                            child: new Text(
+                                "Studio\nbearbeiten", style: new TextStyle(
                                 fontSize: 30.0,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.black),textAlign: TextAlign.center), onPressed: () {})),
+                                color: Colors.black),
+                                textAlign: TextAlign.center),
+                            onPressed: () {})),
                     new Container(
                         margin: new EdgeInsets.all(5.0),
                         color: Colors.grey,
-                        child:new RaisedButton(
-                            child: new Text("Aktion\nerstellen", style: new TextStyle(
+                        child: new RaisedButton(
+                            child: new Text(
+                                "Aktion\nerstellen", style: new TextStyle(
                                 fontSize: 30.0,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.black),textAlign: TextAlign.center), onPressed: () {})),
+                                color: Colors.black),
+                                textAlign: TextAlign.center),
+                            onPressed: () {})),
                     ],)),
                 new Container(
                     margin: new EdgeInsets.all(5.0),
                     color: Colors.grey,
-                    child:new RaisedButton(
+                    child: new RaisedButton(
                         child: new Text("Historie", style: new TextStyle(
                             fontSize: 30.0,
                             fontWeight: FontWeight.bold,
-                            color: Colors.black),textAlign: TextAlign.center), onPressed: () {})),
+                            color: Colors.black), textAlign: TextAlign.center),
+                        onPressed: () {})),
                 new Container(
                     margin: new EdgeInsets.all(5.0),
                     color: Colors.grey,
-                    child:new RaisedButton(
+                    child: new RaisedButton(
                         child: new Text("Statistik", style: new TextStyle(
                             fontSize: 30.0,
                             fontWeight: FontWeight.bold,
-                            color: Colors.black),textAlign: TextAlign.center), onPressed: () {})),
+                            color: Colors.black), textAlign: TextAlign.center),
+                        onPressed: () {})),
                 new Container(
                     margin: new EdgeInsets.all(5.0),
                     color: Colors.grey,
-                    child:new RaisedButton(
+                    child: new RaisedButton(
                         child: new Text("HILFE", style: new TextStyle(
                             fontSize: 30.0,
                             fontWeight: FontWeight.bold,
-                            color: Colors.black),textAlign: TextAlign.center), onPressed: () {})),
+                            color: Colors.black), textAlign: TextAlign.center),
+                        onPressed: () {})),
               ],
               ),
 
@@ -158,73 +169,96 @@ class _MyHomePageState extends State<MyHomePage> {
             ],),),
 
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
 
   Widget _buildExpansionTitle(String time, String typeOfMassage, String name,
-      bool paid, Widget expand) {
-    return new ExpansionTile(
-        title: new Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Container(width: 80.0,
-              child: new Text(time, style: new TextStyle(
-                  fontSize: 20.0, fontWeight: FontWeight.bold)),),
-            new Expanded(
-              child: new Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[new Text(
-                    typeOfMassage, style: new TextStyle(
-                    fontSize: 20.0, fontWeight: FontWeight.bold)),
-                new Text(name, style: new TextStyle(
-                    fontSize: 20.0, fontWeight: FontWeight.bold)),
-                new Row(children: <Widget>[
-                  new Container(margin: new EdgeInsets.only(right: 12.5),
-                    child: new Icon(Icons.face, size: 30.0,),),
-                  (paid ? new Icon(Icons.check_circle_outline) : new Icon(
-                      Icons.radio_button_unchecked)),
-                ],),
+      bool paid, int status, Widget expand) {
+    Color background;
+    if (status == 0) {
+      background = Colors.greenAccent;
+    } else if (status == 1) {
+      background = Colors.redAccent;
+    } else {
+      background = Colors.orangeAccent;
+    }
+    return new Container(color: background,
+        child: new ExpansionTile(
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Container(width: 80.0,
+                  child: new Text(time, style: new TextStyle(
+                      fontSize: 20.0, fontWeight: FontWeight.bold)),),
+                new Expanded(
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[new Text(
+                        typeOfMassage, style: new TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    new Text(name, style: new TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    new Row(children: <Widget>[
+                      new Container(margin: new EdgeInsets.only(right: 12.5),
+                        child: new Icon(Icons.face, size: 30.0,),),
+                      (paid ? new Icon(Icons.check_circle_outline) : new Icon(
+                          Icons.radio_button_unchecked)),
+                    ],),
 
-                ],),),
-          ],),
-        children: <Widget>[
-          expand
-        ]
-    );
+                    ],),),
+              ],),
+            children: <Widget>[
+              expand
+            ]
+        ))
+    ;
   }
 
-  Widget _buildExpandExpandsionTitle() {
-    return new Column(children: <Widget>[new Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        new Container(margin: new EdgeInsets.all(5.0),
-          child: new RaisedButton(
-            child: new Text("Termin Absagen"),
-            onPressed: () {},
-            color: Colors.redAccent,),),
-        new Container(margin: new EdgeInsets.all(5.0),
-          child: new RaisedButton(
-            child: new Text("Termin ändern"),
-            onPressed: () {},
-            color: Colors.orangeAccent,),),
-        new Container(margin: new EdgeInsets.all(5.0),
-          child: new RaisedButton(
-            child: new Text("Bemerkung senden"),
-            onPressed: () {},
-            color: Colors.greenAccent,),),
+  Widget _buildExpandExpandsionTitle(bool isAcepted) {
+    return new Container( //color: Colors.greenAccent[100],
+      child: new Column(children: <Widget>[new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          (isAcepted ? new Container(margin: new EdgeInsets.all(5.0),
+            child: new RaisedButton(
+              child: new Text("Termin Absagen"),
+              onPressed: () {},
+              color: Colors.redAccent,),) :
+          new Container(
+            margin: new EdgeInsets.all(5.0),
+            child: new RaisedButton(
+              child: new Text("Termin Bestätigen"),
+              onPressed: () {},
+              color: Colors.greenAccent,),)),
+          new Container(margin: new EdgeInsets.all(5.0),
+            child: new RaisedButton(
+              child: new Text("Termin ändern"),
+              onPressed: () {},
+              color: Colors.orangeAccent,),),
+          new Container(margin: new EdgeInsets.all(5.0),
+            child: new Text("Status: OK", style: Style.TITLE_LARGE,),),
 
-      ],),
-    new Container(margin: new EdgeInsets.all(5.0),
-        child: new Text(
-          "Bemerkungs Verlauf:\nKunde: Ich bin schlecht n fd jif hidhig jjdshikufdujv bdj, vbh gvbkj fjflkdnlejo ghöfhdizgvkjbcjbclhvjhb chjdh kjfhls hj, hlahl h fhof hs\nStudio: Ich habe sie nicht verstanden.",
-          softWrap: true,)
-    ),
-    ]);
+        ],),
+      new Container(margin: new EdgeInsets.all(5.0),
+          child: new Text(
+            "Bemerkungs Verlauf:\nKunde: Ich bin schlecht n fd jif hidhig jjdshikufdujv bdj, vbh gvbkj fjflkdnlejo ghöfhdizgvkjbcjbclhvjhb chjdh kjfhls hj, hlahl h fhof hs\nStudio: Ich habe sie nicht verstanden.",
+            softWrap: true,)
+      ),
+      new Row(children: <Widget>[
+        new Expanded(
+            child: new TextFormField(decoration: const InputDecoration(
+              hintText: 'Tippe die Nachricht an den Kunden',
+              labelText: 'Nachricht',
+            ),)),
+        new Container(margin: new EdgeInsets.all(5.0),
+          child: new RaisedButton(
+            child: new Text("Senden"),
+            onPressed: () {},
+            color: Colors.blueAccent,),),
+      ],
+      )
+      ],),);
   }
 
   Container _buildSeparation(String text) {
